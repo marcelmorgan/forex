ffe1416a8b5ee93635c24e48da887d84e2a4c69c
Marcel Morgan <marcel.morgan@codedry.com>
2015-02-25 21:50:44 -0500

    Fix URL and fetching method for FGB

--- lib/forex/traders/jm/fgb.rb
+++ lib/forex/traders/jm/fgb.rb
@@ -2,7 +2,7 @@ Forex::Trader.define "FGB" do |t|
   t.base_currency   = "JMD"
   t.name            = "First Global Bank"
   t.twitter_handle  = "@First_Global"
-  t.endpoint        = "http://www.firstglobal-bank.com/"
+  t.endpoint        = "https://www.firstglobal-bank.com/"
 
   t.rates_parser = ->(doc) do # doc is a nokogiri document
     options = {
@@ -13,7 +13,7 @@ Forex::Trader.define "FGB" do |t|
       sell_draft: 3,
     }
 
-    table = doc.css("#forex_rates table").first
+    table = doc.css(".table.table1.small_table").first
     Forex::TabularRates.new(table, options).parse_rates
   end
 end
